fab31a70a37ad7802be2991d9a82417a144c89ed
Víctor Alfonso Mejía Morales <130195315+vamm99@users.noreply.github.com>
2025-01-25 13:13:38 -0500

    agregar PrismaService y actualizar módulos de autenticación para su integración

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './core/prisma.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
